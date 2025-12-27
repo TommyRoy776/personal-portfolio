92d81a6cfccb6e35a42a4f48ee2d70aa99e22922
TommyRoy776 <53387849+TommyRoy776@users.noreply.github.com>
2025-12-26 22:49:57 -0500

    Added button and wheel scroll navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet,RouterLink } from '@angular/router';
-import {AboutMe} from './features/about-me/about-me';
+import { Component, signal, OnInit, HostListener, effect } from '@angular/core';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { AboutMe } from './features/about-me/about-me';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,38 @@ import {AboutMe} from './features/about-me/about-me';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  constructor(private router: Router) {
+    effect(() => {
+      const index = this.pageIndex();
+      this.setPage();
+    });
+  }
+
+  ngOnInit(): void {
+    this.paths = this.router.config
+      .map(r => '/' + r.path)
+      .filter(p => p !== '/undefined');
+    this.pageIndex.set(0);
+  }
+  paths: string[] = [];
+  pageIndex = signal(0);
   protected readonly title = signal('personal-portfolio');
+  setPage() {
+    this.router.navigate([this.paths[this.pageIndex()]]);
+  }
+
+  setPageIndex(deltaY: number) {
+    if (deltaY > 0 && this.pageIndex() > 0) {
+      this.pageIndex.update(i => i - 1);
+    } else if (deltaY < 0 && this.pageIndex() < this.paths.length - 1) {
+      this.pageIndex.update(i => i + 1);
+    }
+
+  }
+  @HostListener('window:wheel', ['$event'])
+  onWheel(event: WheelEvent) {
+    console.log('deltaY:', event.deltaY);
+    this.setPageIndex(event.deltaY)
+  }
 }
